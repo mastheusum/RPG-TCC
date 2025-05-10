1021bbf6e867b1a7d8a24a376517d17ca8d52afd
Victor Matheus <victor.matheus.xd@gmail.com>
2025-05-10 12:30:19 -0300

    Iniciada refatoração do inimigo

--- Scripts/GUI/npc_talk.gd
+++ Scripts/GUI/npc_talk.gd
@@ -15,6 +15,7 @@ func set_talks(data : NPCTalkData):
 
 func remove_talks():
 	visible = false
+	$BG.visible = false
 	self.data = null
 	_index = -1
 	$Delay.stop()
@@ -24,15 +25,18 @@ func remove_talks():
 func start_talk():
 	_index += 1
 	_letters = 0
+	if _index >= data.talks.size():
+		remove_talks()
+		return
 	$BG.visible = true
 	$BG/Label.visible_characters = _letters
 	$BG/Label.text = data.talks[_index]
-	$Delay.start(0.1)
+	$Delay.start(0.05)
 	$Interact.disabled = _index >= data.talks.size()
 
 func _timed():
 	_letters += 1
 	$BG/Label.visible_characters = _letters
 	if _letters < data.talks[_index].length():
-		$Delay.start(0.1)
+		$Delay.start(0.05)
 	
--- Scripts/enemy_base.gd
+++ Scripts/enemy_base.gd
@@ -2,61 +2,86 @@ extends CharacterBody2D
 
 class_name EnemyBase
 
-var daddy : SpawnEnemy
-var target : Node2D = null
+@export_category('Enemy')
 @export var life_max : float = 100
+@export var speed : float = 100.0
+@export var physic_defense : float = 2
+@export var power : float = 5
+@export var patrol_points : Array[Node2D]
 
-var life : float = 0
-var speed = 100.0
-var attack : float = 5
+var _life : float = 0
+var _target : Node2D = null
+var _patrol_index : int = 0
 var _next_pos : Vector2
 
+@onready var daddy : Node2D = get_parent()
 @onready var agent : NavigationAgent2D = $Agent
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	$FollowArea.connect("body_entered", body_entered)
 	$FollowArea.connect("body_exited", body_exited)
+	await get_tree().physics_frame
+	_life = life_max
 
 func body_entered(body : Node2D):
 	if body.is_in_group("Player"):
-		target = body
+		_target = body
 
 func body_exited(body : Node2D):
 	if body.is_in_group("Player"):
-		target = null
+		_target = null
 
 func _physics_process(delta: float) -> void:
-	if life <= 0 or target == null:
+	if _life <= 0:
 		return
-	elif target != null:
-		agent.target_position = target.global_position
 	
-	if not target or agent.is_navigation_finished():
-		return
-		
-	agent.target_position = target.global_position
-	_next_pos = agent.get_next_path_position()
-	
-	velocity = global_position.direction_to(_next_pos) * speed
-	move_and_slide()
+	if not _target:
+		_patrol()
+	elif global_position.distance_to(_target.global_position) >= 50:
+		_pursuit()
+	else:
+		_attack()
 	
 	if velocity.x > 0:
 		sprite.flip_h = false
 	elif velocity.x < 0:
 		sprite.flip_h = true
 	
-	for i in get_slide_collision_count():
-		var collider = get_slide_collision(i).get_collider() as Node2D
-		if collider.is_in_group("Player"):
-			collider = collider as PlayerBase
-			collider.take_damage(attack)
-			dead()
+	#var collider = get_last_slide_collision().get_collider() as Node2D
+	#if collider:
+		#if collider.is_in_group("Player"):
+			#collider = collider as PlayerBase
+			#collider.take_damage(power)
+			#dead()
+
+func _patrol():
+	if patrol_points.size() <= 0:
+		return
+	
+	if agent.is_navigation_finished():
+		agent.target_position = patrol_points[_patrol_index].global_position
+		_patrol_index = (_patrol_index + 1) % patrol_points.size()
+	
+	_next_pos = agent.get_next_path_position()
+	
+	velocity = global_position.direction_to(_next_pos) * speed * 0.7
+	move_and_slide()
+
+func _pursuit():
+	agent.target_position = _target.global_position
+	_next_pos = agent.get_next_path_position()
+	
+	velocity = global_position.direction_to(_next_pos) * speed 
+	move_and_slide()
+
+func _attack():
+	pass
 
 func take_damage(damage):
-	life -= damage
-	if life <= 0:
-		target.gain_exp(1)
+	_life -= damage
+	if _life <= 0:
+		_target.gain_exp(1)
 		dead()
 
 func dead():
@@ -68,4 +93,4 @@ func live():
 	daddy.alive = true
 	$CollisionShape2D.disabled = false
 	$AnimatedSprite2D.visible = true
-	life = life_max
+	_life = life_max
